0b50e7111a5ce2ce0b30d1c4ae95958168d5f585
bijanmahdavi <bijmahdavi@gmail.com>
2019-12-11 16:18:08 -0800

    basic linking of view and controller
    need to fix

--- QuizApp/QuizApp/Controller/ViewController.swift
+++ QuizApp/QuizApp/Controller/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var currentQuestionLabel: UILabel!
+    @IBOutlet weak var quizScoreLabel: UILabel!
+    @IBOutlet weak var progessBarView: UIView!
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    //buttons
+    @IBOutlet weak var buttonA: UIButton!
+    @IBOutlet weak var buttonB: UIButton!
+    @IBOutlet weak var buttonC: UIButton!
+    @IBOutlet weak var buttonD: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    
     }
     
     override var prefersStatusBarHidden: Bool {
         return true
     }
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if sender.tag == 1 {
+            print("button a")
+        } else if sender.tag == 2 {
+            print("button b")
+        } else if sender.tag == 3 {
+            print("button c")
+        } else if sender.tag == 4 {
+            print("button d")
+        }
+    }
+    
 
 }
 
